449d0a027eaab34d296f14ef19629082834998cc
ASMIT SRIVASTAVA <2k22.csai.2211576@gmail.com>
2025-08-31 23:31:14 +0530

    express-change

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // URL-encode the @ symbol in password
+    const conn = await mongoose.connect(
+      process.env.MONGODB_URI || 
+      'mongodb+srv://asmitsrivas0:asm123%40PSIT@cluster0.mcn1orw.mongodb.net/document-summary-db?retryWrites=true&w=majority&appName=Cluster0', 
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
